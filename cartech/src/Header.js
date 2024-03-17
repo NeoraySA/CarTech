3603cc3ef7825ee4f050e2734cb3e9abb646ce6f
Nehoray <neoray3872000@gmail.com>
2024-03-17 15:31:27 +0200

    הוספת קומפוננטת Dashboard ועדכונים נוספים

---
+++ cartech/src/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+      <div className="header">
+        <h2>Header</h2>
+        {/* ניתן להוסיף כאן תפריט ניווט או לוגו */}
+      </div>
+    );
+  }
+  
+  export default Header;
+  
