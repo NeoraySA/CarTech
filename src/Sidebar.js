6a48cb425d2b5adf26ae470ace5acbf571699c9d
USER <USER@NEHORAYPC>
2024-03-22 03:02:53 +0200

    הוספת קוד לתצוגה ניידת של תפריט צידי

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,34 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCar, faUsers, faCalendarAlt, faHandshake, faBook, faCog, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-
+import { faCar, faUsers, faCalendarAlt, faHandshake, faBook, faCog, faChevronLeft, faTimes } from '@fortawesome/free-solid-svg-icons';
 import './Sidebar.css';
 
-function Sidebar({ isSidebarOpen, toggleSidebar }) {
+function Sidebar({ toggleSidebar }) {
+  const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState(false);
+
   const links = [
     { name: "דשבורד", icon: faBook, path: "/dashboard" },
     { name: "הוספת לקוח", icon: faUsers, path: "/addCustomer" },
     { name: "מידע על רכב", icon: faCar, path: "/vehicleInfo" },
-    { name: "לקוחות", icon: faUsers, path: "/clients" },
-    { name: "רכבים", icon: faCar, path: "/cars" },
-    { name: "השכרות", icon: faCalendarAlt, path: "/rentals" },
-    { name: "מכירות", icon: faHandshake, path: "/sales" },
-    { name: "חשבונאות", icon: faBook, path: "/accounting" },
-    { name: "הגדרות", icon: faCog, path: "/settings" },
+    // רשימת הקישורים נמשכת...
   ];
 
+  const handleMobileToggle = () => {
+    setIsMobileSidebarOpen(!isMobileSidebarOpen);
+    toggleSidebar(); // קריאה לפונקציה toggleSidebar המועברת ב-props
+  };
+
   return (
-    <div className={`sidebar ${isSidebarOpen ? "open" : "close"}`}>
+    <div className={`sidebar ${isMobileSidebarOpen ? "open" : "close"}`}>
       <div className="sidebar-content">
         {links.map(({ name, icon, path }, index) => (
           <a key={index} href={path} className="sidebar-link">
             <FontAwesomeIcon icon={icon} />
-            {isSidebarOpen && <span>{name}</span>}
+            <span>{name}</span>
           </a>
         ))}
       </div>
-      <button onClick={toggleSidebar} className="sidebar-toggle">
-        {isSidebarOpen ? <FontAwesomeIcon icon={faChevronLeft} /> : <FontAwesomeIcon icon={faChevronRight} />}
+      <button onClick={handleMobileToggle} className="mobile-sidebar-toggle">
+        {isMobileSidebarOpen ? <FontAwesomeIcon icon={faTimes} /> : <FontAwesomeIcon icon={faChevronLeft} />}
       </button>
     </div>
   );
